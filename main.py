94883ffca7ad1152ceea261755f79e51370fa1ba
Mahirul <40130645+Mahir-Islam@users.noreply.github.com>
2021-10-08 14:28:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -80,33 +80,6 @@ def donothing():
    
 menubar = Menu(root)
 
-"""
-filemenu = Menu(menubar, tearoff=0) #File
-filemenu.add_command(label="New", command=donothing)
-filemenu.add_command(label="Open", command=donothing)
-filemenu.add_command(label="Save", command=donothing)
-filemenu.add_command(label="Save as...", command=donothing)
-filemenu.add_command(label="Close", command=donothing)
-filemenu.add_separator()
-filemenu.add_command(label="Exit", command=root.quit)
-menubar.add_cascade(label="File", menu=filemenu) #File
-
-editmenu = Menu(menubar, tearoff=0)  #Edit
-editmenu.add_command(label="Undo", command=donothing)
-editmenu.add_separator()
-editmenu.add_command(label="Cut", command=donothing)
-editmenu.add_command(label="Copy", command=donothing)
-editmenu.add_command(label="Paste", command=donothing)
-editmenu.add_command(label="Delete", command=donothing)
-editmenu.add_command(label="Select All", command=donothing)
-menubar.add_cascade(label="Edit", menu=editmenu) #Edit
-
-helpmenu = Menu(menubar, tearoff=0) #Help
-helpmenu.add_command(label="Help Index", command=donothing)
-helpmenu.add_command(label="About...", command=donothing)
-menubar.add_cascade(label="Help", menu=helpmenu) #Help
-"""
-
 analysis = Menu(menubar, tearoff=0) #Analysis
 
 analysis.add_checkbutton(label="Standard Deviation", onvalue=1, offvalue=0, variable=SD)
